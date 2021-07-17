a159a8589a5630dca7b7b668cbeeaff557b5d495
GerardoCP-PE <77426568+GerardoCP-PE@users.noreply.github.com>
2021-07-16 20:32:11 -0500

    Update db.js

--- node_api/db.js
+++ node_api/db.js
@@ -1,6 +1,5 @@
 const db = require("mongoose");
 db.Promise = global.Promise;
-//"mongodb+srv://db_user_demo:I1jetZVwryfALGJT@cluster0.w7qgp.mongodb.net/nodejsdemo_db?retryWrites=true&w=majority"
 async function connect(url) {
   await db.connect(
     url,
